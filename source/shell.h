7ac281c146d44cf426a2bdb49f1812438c80beb5
Henry Ong <67896884+100sonly@users.noreply.github.com>
2024-07-03 08:25:18 +0800

    added function references to tts

--- source/shell.h
+++ source/shell.h
@@ -17,7 +17,9 @@ const char *builtin_commands[] = {
     "usage", // Provides a brief usage guide for the shell and its built-in command
     "env", // Lists all the environment variables currently set in the shell
     "setenv", // Sets or modifies an environment variable for this shell session
-    "unsetenv" // Removes an environment variable from the shell
+    "unsetenv", // Removes an environment variable from the shell
+    "tts"   //Text to speech mode
+
     };
 
 /*
@@ -30,16 +32,19 @@ int shell_usage(char **args);
 int list_env(char **args);
 int set_env_var(char **args);
 int unset_env_var(char **args);
-
+int shell_tts(char **args);
+void tts(const char *text);
 /*** This is array of functions, with argument char ***/
 int (*builtin_command_func[])(char **) = {
     &shell_cd,     // builtin_command_func[0]: cd
     &shell_help,   // builtin_command_func[1]: help
     &shell_exit,   // builtin_command_func[2]: exit
     &shell_usage,  // builtin_command_func[3]: usage
-    &list_env,     // builtin_command_func[4]: env
+    &list_env,     // builtin_command_func[4]: envA
     &set_env_var,  // builtin_command_func[5]: setenv
-    &unset_env_var // builtin_command_func[6]: unsetenv
+    &unset_env_var, // builtin_command_func[6]: unsetenv
+    &shell_tts   // builtin_command_func[7]: tts
+
 };
 
 // Helper function to figure out how many builtin commands are supported by the shell
